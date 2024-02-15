3bc2305efc1ae522a857f35ed2a547ca53afc044
DhanushreeTZ <159924766+DhanushreeTZ@users.noreply.github.com>
2024-02-15 15:08:21 +0530

    Update multi-assign.go

--- variable/multi-assign.go
+++ variable/multi-assign.go
@@ -1,1 +1,12 @@
+// multiple assignment
 
+package main
+import "fmt"
+func main() {
+    var(
+        name string
+        number int
+        )
+    name, number = "Dhanu", 3
+    fmt.Println("Name:", name, "Number: ", number)
+}
