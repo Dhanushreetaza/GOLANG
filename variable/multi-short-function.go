29c28d3c8b51086376e2dd91ee6429d2e0d9aba8
DhanushreeTZ <159924766+DhanushreeTZ@users.noreply.github.com>
2024-02-16 09:31:41 +0530

    Update multi-short-function.go

--- variable/multi-short-function.go
+++ variable/multi-short-function.go
@@ -1,1 +1,13 @@
+// multiple functions
+// _ is blank identifier
+// int, int
 
+package main
+import "fmt"
+func main() {
+    _, a:= multi()
+    fmt.Println(a)
+}
+func multi() (int, int) {
+    return 10,5
+}
