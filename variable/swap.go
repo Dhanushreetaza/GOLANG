93fd4d751ee17dc88d819fa384cb9044c3d99fa1
DhanushreeTZ <159924766+DhanushreeTZ@users.noreply.github.com>
2024-02-16 09:38:18 +0530

    Update swap.go

--- variable/swap.go
+++ variable/swap.go
@@ -1,1 +1,11 @@
+// to swap values
+// use := and = correctly
+
+package main
+import "fmt"
+func main() {
+    subject1, subject2 := "math" , " english"
+    subject1, subject2 = "Science" , "History"
+    fmt.Println(subject1, subject2 )
+}
 
