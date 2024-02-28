0cc0d92b48bd3099d0b93daad97c344c6c13955d
DhanushreeTZ <159924766+DhanushreeTZ@users.noreply.github.com>
2024-02-28 13:13:47 +0530

    Create crash.go

---
+++ pointer/crash.go
@@ -0,0 +1,18 @@
+//identify and print the brand causing the crash
+package main
+
+import "fmt"
+
+type computer struct {
+	brand *string
+}
+
+func main() {
+	var c *computer
+	change(c, "apple")
+	fmt.Printf("brand: %s\n", c.brand)
+}
+
+func change(c *computer, brand string) {
+	(*c.brand) = brand
+}
